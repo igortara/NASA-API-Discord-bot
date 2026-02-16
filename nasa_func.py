1e86fae2f2e16dc8e33fa83d58b0009f9091ddc3
FeliksDev <153300395+igortara@users.noreply.github.com>
2026-02-16 22:48:55 +0200

    first commit

---
+++ nasa_func.py
@@ -0,0 +1,45 @@
+# --- Importing necessary libraries ---
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+
+# --- Functions ---
+def get_nasa_image(type):
+    today = datetime.now().strftime("%Y-%m-%d")
+    ext = None
+
+    if type == "FIRMS":
+        ext = "VIIRS_SNPP_Thermal_Anomalies_375m_All"
+    elif type == "TrueColor":
+        ext = "VIIRS_SNPP_CorrectedReflectance_TrueColor"
+
+    args = {
+        "SERVICE": "WMS",
+        "VERSION": "1.3.0",
+        "REQUEST": "GetMap",
+        "LAYERS": f"{ext}",
+        "STYLES": "",
+        "FORMAT": "image/jpeg",
+        "CRS": "EPSG:4326",
+        "WIDTH": "1920",
+        "HEIGHT": "1080",
+        "BBOX": "30, -10, 70, 60", 
+        "TIME": today
+    }
+    resp = requests.get("https://gibs.earthdata.nasa.gov/wms/epsg4326/best/wms.cgi?", params=args)
+    if resp.status_code == 200:
+        with open("nasa_wallpaper.jpg", "wb") as f:
+            f.write(resp.content)
+        return "nasa_wallpaper.jpg"
+    else: 
+        return None
+
+
+def get_apod_image():
+    apod_url = "https://apod.nasa.gov/apod/astropix.html"
+    soup = BeautifulSoup(requests.get(apod_url).content, "html.parser")
+    find_img = soup.find("img")
+    if find_img is not None:
+        return f"https://apod.nasa.gov/apod/{find_img['src']}"
+    else:
+        return None
---
+++ nasa_walpaper.py
@@ -0,0 +1,60 @@
+# -- NASA API Bot by Feliks --
+# ----------------------------
+# -- Imports --
+import nasa_func
+import os
+import discord
+from discord.ext import commands 
+from dotenv import load_dotenv
+import datetime
+load_dotenv()
+# -- Init --
+apod_url = "https://apod.nasa.gov/apod/astropix.html"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+bot = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+today = None
+# -- NASA satelite image for today function --
+@bot.command()
+async def nasa_sat(ctx):
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    image_url = nasa_func.get_nasa_image(type="TrueColor")
+    if image_url is None:
+        await ctx.send("Sorry, I couldn't fetch the NASA satelite image for today.")
+        return
+    embded = discord.Embed(title="NASA Satelite Image", description=f"Here is the NASA satelite image for {today}!", color=0x0035a9)
+    embded.set_image(url="attachment://nasa_wallpaper.jpg")
+    await ctx.send(embed=embded , file=discord.File(image_url))
+    os.remove("nasa_wallpaper.jpg")
+# -- NASA FIRMS image for today function --
+@bot.command()
+async def fires(ctx):
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    image_url = nasa_func.get_nasa_image(type="FIRMS")
+    if image_url is None:
+        await ctx.send("Sorry, I couldn't fetch the NASA FIRMS image for today.")
+        return
+    embded = discord.Embed(title="NASA FIRMS Image", description=f"Here is the NASA FIRMS image for {today}!", color=0x0035a9)
+    embded.set_image(url="attachment://nasa_wallpaper.jpg")
+    await ctx.send(embed=embded , file=discord.File(image_url))
+    os.remove("nasa_wallpaper.jpg")   
+# -- Help function --
+@bot.command()
+async def helpme(ctx):
+    help_text = """
+    **!nasa_sat** - Fetches the NASA satellite image for today.
+    **!apod** - Fetches the Astronomy Picture of the Day (APOD) image for today.
+    **!helpme** - Displays this help message.
+    """
+    await ctx.send(help_text)
+# -- NASA APOD image for today function --
+@bot.command()
+async def apod(ctx):
+    embed = discord.Embed(title="NASA APOD", description="Here is today's Astronomy Picture of the Day (APOD)!", color=0x0035a9)
+    image_url = nasa_func.get_apod_image()
+    if image_url is not None:
+        embed.set_image(url=image_url)
+        await ctx.send(embed=embed)
+
+# -- Run the bot --
+if __name__ == "__main__":
+    bot.run(BOT_TOKEN)
